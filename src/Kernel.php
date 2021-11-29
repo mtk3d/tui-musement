6b52ce4c9bd2e977a816a3631b751e0776b578da
Mateusz Cholewka <mateusz@cholewka.com.pl>
2021-11-29 16:45:34 +0100

    feature: add test service configuration

--- src/Kernel.php
+++ src/Kernel.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace TuiMusement;
 
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Container;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
@@ -12,19 +13,33 @@ class Kernel
 {
     private ContainerBuilder $container;
 
-    public function __construct()
+    public function __construct(string $env = 'prod')
     {
         $this->container = new ContainerBuilder();
         $loader = new YamlFileLoader($this->container, new FileLocator(__DIR__ . '/../config'));
         $loader->load('services.yml');
         $loader->load('parameters.yml');
+        if ('test' === $env) {
+            $loader->load('services_test.yml');
+        }
         $this->container->compile();
+    }
+
+    public function container(): Container
+    {
+        return $this->container;
+    }
+
+    public function app(): Application
+    {
+        /** @var Application $app */
+        $app = $this->container()->get(Application::class);
+
+        return $app;
     }
 
     public function run(): void
     {
-        /** @var Application $app */
-        $app = $this->container->get(Application::class);
-        $app->run();
+        $this->app()->run();
     }
 }
---
+++ test/Integration/CityWeather/ListCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TuiMusement\CityWeather;
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\Console\Tester\CommandTester;
+use TuiMusement\Application;
+use TuiMusement\Kernel;
+
+class ListCommandTest extends TestCase
+{
+    private Application $application;
+
+    public function setUp(): void
+    {
+        $kernel = new Kernel('test');
+        $this->application = $kernel->app();
+    }
+
+    public function testRunningCommand(): void
+    {
+        $command = $this->application->find('list:cities');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute([]);
+
+        $output = $commandTester->getDisplay();
+        $this->assertStringContainsString('Processed city Warsaw | Rainy - Sunny', $output);
+    }
+}
